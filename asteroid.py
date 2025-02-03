ad82767c9bc725cfbdef411afc6f73ce866f7e0b
mr_rambling <zwhiteley@gmail.com>
2025-02-04 10:31:30 +1100

    Working game and scoring

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from circleshape import CircleShape
+from constants import *
 
 
 class Asteroid(CircleShape):
@@ -19,3 +21,18 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        # Create new split asteroids
+        rotation = random.uniform(20, 50)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        split1 = Asteroid(self.position.x, self.position.y, new_radius)
+        split1.velocity = self.velocity.rotate(rotation) * 1.2
+
+        split2 = Asteroid(self.position.x, self.position.y, new_radius)
+        split2.velocity = self.velocity.rotate(-rotation) * 1.2
--- main.py
+++ main.py
@@ -3,20 +3,24 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     timer = pygame.time.Clock()
+    score = 0
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (updatable, drawable, shots)
 
     player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) 
     field = AsteroidField()
@@ -40,10 +44,16 @@ def main():
         # limit the frame rate to 60 FPS
         dt = timer.tick(60) / 1000
 
-        for obj in asteroids:
-            if obj.collision(player1):
+        for asteroid in asteroids:
+            if asteroid.collision(player1):
                 print("Game over!")
+                print(f"Score: {score}")
                 return
+            for shot in shots:
+                if shot.collision(asteroid):
+                    shot.kill()
+                    asteroid.split()
+                    score += 1
 
 if __name__ == "__main__":
     main()
